099ce6364612042ff40fe5e195369c81e15b5ef6
Subhankar Bhattacharyya <subhankar99@gamil.com>
2025-01-01 18:42:50 +0530

    final commit

--- src/Api/AxiosInstance/AxiosInstance.js
+++ src/Api/AxiosInstance/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_APP_BASE_URL,
+  baseURL: process.env.REACT_APP_BASE_URL,
 });
 
 axiosInstance.interceptors.request.use(
--- src/Pages/EmailVerification.jsx
+++ src/Pages/EmailVerification.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axiosInstance from "../Api/AxiosInstance/AxiosInstance";
+import axios from "axios";
 import { toast } from "react-toastify";
 
 const EmailVerification = () => {
